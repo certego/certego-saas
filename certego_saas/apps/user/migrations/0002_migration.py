e555df5104269f9a6e6da640baf4386902c7f82b
0ssigeno <simone.berni2@studio.unibo.it>
2022-07-05 15:44:55 +0200

    Black

--- certego_saas/apps/user/migrations/0002_migration.py
+++ certego_saas/apps/user/migrations/0002_migration.py
@@ -9,15 +9,27 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            sql=["ALTER TABLE IF EXISTS certego_saas_user RENAME TO certego_saas_user_user;"],
-            reverse_sql=["ALTER TABLE IF EXISTS certego_saas_user_user RENAME TO certego_saas_user;"],
+            sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user RENAME TO certego_saas_user_user;"
+            ],
+            reverse_sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user_user RENAME TO certego_saas_user;"
+            ],
         ),
         migrations.RunSQL(
-            sql=["ALTER TABLE IF EXISTS certego_saas_user_groups RENAME TO certego_saas_user_user_groups;"],
-            reverse_sql=["ALTER TABLE IF EXISTS certego_saas_user_user_groups RENAME TO certego_saas_user_groups;"],
+            sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user_groups RENAME TO certego_saas_user_user_groups;"
+            ],
+            reverse_sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user_user_groups RENAME TO certego_saas_user_groups;"
+            ],
         ),
         migrations.RunSQL(
-            sql=["ALTER TABLE IF EXISTS certego_saas_user_user_permissions RENAME TO certego_saas_user_user_user_permissions;"],
-            reverse_sql=["ALTER TABLE IF EXISTS certego_saas_user_user_user_permissions RENAME TO certego_saas_user_user_permissions;"],
+            sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user_user_permissions RENAME TO certego_saas_user_user_user_permissions;"
+            ],
+            reverse_sql=[
+                "ALTER TABLE IF EXISTS certego_saas_user_user_user_permissions RENAME TO certego_saas_user_user_permissions;"
+            ],
         ),
     ]

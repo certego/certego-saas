e66cee41aaddf086fb002a2b1d04c97aeb2b596c
0ssigeno <simone.berni2@studio.unibo.it>
2022-07-05 14:51:44 +0200

    Fix urls

--- certego_saas/urls.py
+++ certego_saas/urls.py
@@ -1,4 +1,15 @@
 # Patterns
-urlpatterns = [
+from django.urls import include, path
 
+from django.apps import apps
+
+urlpatterns = [
 ]
+
+if apps.is_installed("certego_saas.apps.user"):
+    # certego_saas: user sub-app
+    urlpatterns.append(
+        path("", include("certego_saas.apps.user.urls")),
+    )
+
+

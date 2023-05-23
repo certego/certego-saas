4bc47eb5d2358aa59aa52b7adf30df484b32bead
0ssigeno <s.berni@certego.net>
2023-05-23 10:54:53 +0200

    Middleware

--- certego_saas/ext/middlewares.py
+++ certego_saas/ext/middlewares.py
@@ -1,4 +1,10 @@
+import logging
 import time
+
+from django.conf import settings
+from django.http.response import Http404
+
+logger = logging.getLogger(__name__)
 
 
 class StatsMiddleware:
@@ -13,11 +19,6 @@ class StatsMiddleware:
     def __call__(self, request):
         request.x_request_time = time.time()  # used in exception logging
         return self.get_response(request)
-
-
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class LogMiddleware:
@@ -39,6 +40,31 @@ class LogMiddleware:
         response = self.get_response(request)
 
         logger.info(f"response {request.method} {request.path} sent to {request.user}")
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+
+class BlockListForwardedForMiddleware:
+    """
+    Every request that has the X-Forwarded-From set as an ip in BLOCKLIST_FORWARDED_FOR returns 404
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        remote_addr = request.headers.get("X-Forwarded-For", "")
+        if remote_addr in settings.BLOCKLIST_FORWARDED_FOR:
+            raise Http404(f"Request has X-Forwarded-For set to {remote_addr}: blocked")
+
+        response = self.get_response(request)
 
         # Code to be executed for each request/response after
         # the view is called.

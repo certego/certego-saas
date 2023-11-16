2d368c5b491657f58e8e74730e781e94eefb9ec1
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-11-16 15:40:23 +0100

    added indexes for BIDocument

--- certego_saas/ext/upload/elastic.py
+++ certego_saas/ext/upload/elastic.py
@@ -69,7 +69,7 @@ try:
     from mongoengine import fields as mongo_fields
 
 except ImportError:
-    from django.db.models import JSONField, Model
+    from django.db.models import Index, JSONField, Model
     from django.db.models import fields as django_fields
 
     class BIDocument(__BIDocumentInterface, Model):
@@ -78,6 +78,16 @@ except ImportError:
         category = django_fields.CharField(max_length=100)
         count = django_fields.PositiveIntegerField()
         kwargs = JSONField()
+
+        class Meta:
+            indexes = [
+                Index(
+                    fields=[
+                        "index",
+                        "time",
+                    ]
+                )
+            ]
 
 else:
 
@@ -89,3 +99,4 @@ else:
         category = mongo_fields.StringField(required=True)
         count = mongo_fields.IntField(required=True, min_value=0)
         kwargs = mongo_fields.DictField(required=False)
+        meta = {"indexes": ["index", "time"]}
